e06153c8a377c4c756928a76ae626a5a3a336a90
Lautaro <lmendezayl@gmail.com>
2023-04-25 22:54:00 -0300

    hasta el 10 inclusive

--- soluciones/guia_4.hs
+++ soluciones/guia_4.hs
@@ -21,8 +21,7 @@ esDivisible n x | n == 0 = True
 -- Ejercicio 4 
 sumaImpares :: Integer -> Integer
 sumaImpares n | n == 0 = 0
-              | even n = sumaImpares (n-1)
-              | otherwise = (sumaImpares (n-1)) + n
+              | otherwise = (sumaImpares (n-1)) + (2*n-1)
 -- Ejercicio 5 
 medioFact :: Integer ->Integer
 medioFact n | n == 0  || n == (-1) = 1
@@ -33,6 +32,9 @@ sumaDigitos :: Integer ->Integer
 sumaDigitos 0 = 0
 sumaDigitos n = mod n 10 + sumaDigitos (div n 10)
 
+--mod n 10 → me da el ultimo digito de n.
+--div n 10 → le saca el ultimo digito a n
+
 -- Ejercicio 7
 todosDigitosIguales :: Integer -> Bool
 todosDigitosIguales n | n == mod n 10 = True
@@ -41,8 +43,33 @@ todosDigitosIguales n | n == mod n 10 = True
 
 -- Ejercico 8 
 iesimoDigito :: Integer -> Integer -> Integer
-iesimoDigito n i = mod (div n (10^(cantDigitos n - i))) 10
+iesimoDigito n i | i == cantDigitos n = mod n 10
+                 | otherwise = iesimoDigito (div n 10 ) i
 
 cantDigitos :: Integer -> Integer
-cantDigitos x | x == 0 = 0
-              | otherwise = 1 + cantDigitos (div x 10)
+cantDigitos n | n < 10 = 1
+              | otherwise = 1 + cantDigitos (div n 10)
+
+--Ejercicio 9 
+esCapicua :: Integer -> Bool
+esCapicua n = (show n) == reverse (show n)
+
+--Ejercicio 10
+--10a 
+f1 :: Integer -> Integer 
+f1 0 = 1
+f1 n = (-1) + 2^(n + 1)
+
+--10b
+f2 :: Integer -> Float -> Float
+f2 n q | n == 1 = q
+       | otherwise = q^n  + f2 (n - 1) q
+
+--10c
+f3 :: Integer -> Float -> Float
+f3 n q | n == 1 = q^2 + q 
+       | otherwise = q^(2*n) + q^(2*n - 1) + f3 (n - 1) q
+
+--10d
+f4 :: Integer -> Float -> Float 
+f4 n q = f3 n q - f2 (n-1) q
