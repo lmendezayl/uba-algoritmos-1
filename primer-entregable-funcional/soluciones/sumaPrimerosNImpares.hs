3fb09918b39e5f5eeddb4134f081870b992152d7
Lautaro Mendez <81045740+lmendezayl@users.noreply.github.com>
2023-04-27 11:34:37 -0300

    Update con respuesta sumaPrimerosNImpares.hs

--- primer-entregable-funcional/soluciones/sumaPrimerosNImpares.hs
+++ primer-entregable-funcional/soluciones/sumaPrimerosNImpares.hs
@@ -7,7 +7,8 @@ main = do {
 
 sumaPrimerosNImpares :: Integer -> Integer
 -- Completar la definición de la función
-
+sumaPrimerosNImpares n | n < 1 = undefined
+                       | otherwise = sum [if (mod i 2 == 0) then 0 else 2*i + 2 | i <- [1..(2*n - 1)]]
 -- Pueden agregan las funciones que consideren necesarias
 
 
