c73af0115ee87ac68870d647c648cb88be76ce44
Lautaro <lmendezayl@gmail.com>
2023-05-23 19:44:44 -0300

    hasta el 1.5

--- guias/soluciones/guia8.py
+++ guias/soluciones/guia8.py
@@ -24,12 +24,45 @@ def divideATodos(s: list([int]), e: int) -> bool:
     while (i <= len(s)):
         if (s[i] % e == 0):
             i += 1
-        else:
-            break
+        else: 
+            return False
+    return True
         
 def divideATodos2(s: list, e: int):
     for i in s:
         if i % e == 0:            
             return True
         else:
-            return False                      
+            return False
+
+
+# 1.3
+def sumaTotal (lista: list) -> int:
+    total = 0
+    for elemento in lista:
+        total += elemento
+    return total
+
+# 1.4
+def ordenados(lista: list[int]) -> bool:
+    elementoAnterior: int = lista[0]
+    lista = lista[1::] 
+    for elemento in lista:
+        if elementoAnterior >= elemento:
+            return False
+        else:
+            elementoAnterior = elemento
+    return True
+    
+# 1.5
+def palabraMayorASiete(lista: list[str]) -> bool:
+    for elemento in lista:
+        if len(elemento) > 7:
+            return True
+        else:
+    return True
+
+listaStr = ["hola","elpepe"]
+listaStr2 = ["holwwwwwa","elpepe"]
+print(palabraMayorASiete(listaStr))
+print(palabraMayorASiete(listaStr2))
